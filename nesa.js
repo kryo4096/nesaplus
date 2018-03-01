2473d768388002de824a3cb6e24432e6d1278a91
EnigmaticCrow <1.jmaier.3@gmail.com>
2018-03-01 12:40:32 +0100

    Update nesa.js

--- nesa.js
+++ nesa.js
@@ -7,6 +7,8 @@ if(table) console.log("Found Table");
 let n = 0.0;
 let sum = 0.0;
 let points = 0;
+let sSum = 0;
+let m = 0;
 
 // iterate through rows
 for(let i = 0; i < table.rows.length - 1; i++) {
@@ -22,17 +24,28 @@ for(let i = 0; i < table.rows.length - 1; i++) {
     // averaging
     n++;
     sum += grade
-
-    //check if grade is "genügend"
-    if(grade>=4.0){
-      // add points with a multiplier of 1
-      points += Math.round((grade-4)*2)/2*1;
+    
+    if(table.rows[i].cells[0].innerText.startsWith("s") {
+       // Schwerpunkt/Focus lessons count together
+       sSum += grade;
+       m++;
     } else {
-      // add points with a multiplier of 2
-      points += Math.round((grade-4)*2)/2*2;
+      //check if grade is "genügend"
+      if(grade>=4.0){
+        // add points with a multiplier of 1
+        points += Math.round((grade-4)*2)/2*1;
+      } else {
+        // add points with a multiplier of 2
+        points += Math.round((grade-4)*2)/2*2;
+      }
     }
   }
 }
-
+let sAverage = sSum / m;
+if(sAverage >= 4.0) {
+  points += Math.round((sAverage-4)*2)/2*1;
+} else {
+  points += Math.round((sAverage-4)*2)/2*2;
+}
 // show result
 table.insertRow(table.rows.length).innerHTML = "<td style=\"border-bottom: 0; padding-top: 2mm;\"><a href='https://github.com/kryo4096/nesaplus'>NesaPlus</a><i></i></td><td style=\"border-bottom: 0; padding-top: 2mm;\">Pluspunkte: " + points + "</td><td style=\"border-bottom: 0; padding-top: 2mm;\">Durchschnitt: " + Math.round(sum/n*1000)/1000 + "</td><td style=\"border-bottom: 0; padding-top: 2mm;\"> </td><td style=\"border-bottom: 0; padding-top: 2mm;\"> </td>";
